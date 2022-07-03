798d81ee8ee447fe29d83c7386665e3520f616f8
SahilK-027 <104154041+SahilK-027@users.noreply.github.com>
2022-07-03 13:13:07 +0530

    Update singly-linked-list.c
    C program for singly-linked-list

--- singly-linked-list.c
+++ singly-linked-list.c
@@ -121,8 +121,6 @@ int main()
         printf("What do you want to perform?\n");
         printf("ENTER YOUR CHOISE\n1. Insert At Start\n2. Insert At End\n3. DISPLAY LINKED LIST\n4. Delete First node\n5. Delete Last Node\n6. Exit\n");
         scanf("%d",&ch);
-        if(ch>=1 && ch<=6)
-        {
         switch(ch)
         {
         case 1:
@@ -147,11 +145,8 @@ int main()
         case 6:
                 flag=0;
                 break;
-        }
-        }
-        else
-        {
-            printf("\n🚫Incorrect choice Re-Enter🚫\n\n");
+       default:
+                printf("\n🚫Incorrect choice Re-Enter🚫\n\n");    
         }
     }
     return 0;
